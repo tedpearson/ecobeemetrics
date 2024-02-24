00425e156273f8d030a28ac56c56c7242106f6ca
Ted Pearson <1477390+tedpearson@users.noreply.github.com>
2024-02-23 23:02:35 -0500

    make influx points immediately with no intermediate representation
    - this removes the need for reflection code and structs
    - also removing program and equipment status string fields; i am not using them anymore, as victoriametrics only supports numeric fields.
    - remove use of pkg/errors, it's not providing any value.
    - use fmt.Printf instead of log.Printf, I'm running this via systemd which has its own log timestamps.

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/go-co-op/gocron/v2"
-	"github.com/iancoleman/strcase"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
-	"github.com/pkg/errors"
 	"github.com/rspier/go-ecobee/ecobee"
 	"gopkg.in/yaml.v3"
 )
@@ -41,7 +38,7 @@ func main() {
 	if *getPin {
 		pinResponse, err := ecobee.Authorize(config.Ecobee.AppId)
 		if err != nil {
-			log.Fatalf("Error authorizing with ecobee: %+v", errors.WithStack(err))
+			log.Fatalf("Error authorizing with ecobee: %v", err)
 		}
 		fmt.Printf("Ecobee PIN: %s\n", pinResponse.EcobeePin)
 		fmt.Printf("Ecobee code: %s\n", pinResponse.Code)
@@ -57,7 +54,6 @@ func main() {
 
 	// create ecobee client
 	e := ecobee.NewClient(config.Ecobee.AppId, config.Ecobee.AuthCacheFile)
-	id := config.Ecobee.ThermostatId
 
 	// create influx client
 	ic := config.InfluxDB
@@ -70,7 +66,7 @@ func main() {
 	}
 	_, err = s.NewJob(
 		gocron.CronJob(config.Ecobee.PollCron, false),
-		gocron.NewTask(run, e, write, id, config),
+		gocron.NewTask(run, e, write, config),
 		gocron.WithSingletonMode(gocron.LimitModeReschedule),
 	)
 	if err != nil {
@@ -83,10 +79,11 @@ func main() {
 
 var lastRevision string
 
-func run(e *ecobee.Client, writeApi api.WriteAPIBlocking, id string, config Config) {
+func run(e *ecobee.Client, writeApi api.WriteAPIBlocking, config Config) {
 	now := time.Now()
+	id := config.Ecobee.ThermostatId
 
-	tsm, err := e.GetThermostatSummary(
+	summaries, err := e.GetThermostatSummary(
 		ecobee.Selection{
 			SelectionType:          "thermostats",
 			SelectionMatch:         id,
@@ -96,13 +93,12 @@ func run(e *ecobee.Client, writeApi api.WriteAPIBlocking, id string, config Conf
 		log.Printf("error retrieving thermostat s for %s: %v", id, err)
 		return
 	}
-	s := tsm[id]
+	summary := summaries[id]
 
-	ts, err := e.GetThermostats(ecobee.Selection{
+	thermostats, err := e.GetThermostats(ecobee.Selection{
 		SelectionType:  "thermostats",
-		SelectionMatch: config.Ecobee.ThermostatId,
+		SelectionMatch: id,
 		IncludeRuntime: true,
-		IncludeProgram: true,
 		IncludeEvents:  true,
 		IncludeSensors: true,
 	})
@@ -110,164 +106,84 @@ func run(e *ecobee.Client, writeApi api.WriteAPIBlocking, id string, config Conf
 		log.Printf("error retrieving thermostat: %v", err)
 		return
 	}
+	thermostat := thermostats[0]
 
-	t := ts[0]
-	if s.RuntimeRevision == lastRevision {
+	if summary.RuntimeRevision == lastRevision {
 		log.Println("--- No Update ---")
 	} else {
-		log.Printf("--- Got updated data on thermostat and %d sensors from Ecobee ---", len(t.RemoteSensors))
-	}
-	therm := ThermostatFields{
-		CoolingSetpoint: float64(t.Runtime.DesiredCool) / 10,
-		HeatingSetpoint: float64(t.Runtime.DesiredHeat) / 10,
-		Program:         Program(t),
-		EquipmentStatus: EquipmentStatus(s.EquipmentStatus),
-		Heat1:           s.HeatPump,
-		Heat2:           s.HeatPump2,
-		Heat3:           s.HeatPump3,
-		Cool1:           s.CompCool1,
-		Cool2:           s.CompCool2,
-		AuxHeat1:        s.AuxHeat1,
-		AuxHeat2:        s.AuxHeat2,
-		AuxHeat3:        s.AuxHeat3,
-		Fan:             s.Fan,
-		Idle: !s.HeatPump && !s.HeatPump2 && !s.HeatPump3 && !s.CompCool1 && !s.CompCool2 && !s.AuxHeat1 &&
-			!s.AuxHeat2 && !s.AuxHeat3 && !s.Fan,
+		log.Printf("--- Got updated data on thermostat and %d sensors from Ecobee ---", len(thermostat.RemoteSensors))
 	}
 
-	sensors := make([]Sensor, len(t.RemoteSensors)+1)
-	for i, s := range t.RemoteSensors {
-		var temp float64
-		var occ bool
-		var hum *float64
-		var name string
+	thermostatPoint := write.NewPointWithMeasurement(config.InfluxDB.Measurements.Thermostat).
+		SetTime(now).
+		AddTag("name", thermostat.Name).
+		AddField("cooling_setpoint", float64(thermostat.Runtime.DesiredCool)/10).
+		AddField("heating_setpoint", float64(thermostat.Runtime.DesiredHeat)/10).
+		AddField("heat1", summary.HeatPump).
+		AddField("heat2", summary.HeatPump2).
+		AddField("heat3", summary.HeatPump3).
+		AddField("cool1", summary.CompCool1).
+		AddField("cool2", summary.CompCool2).
+		AddField("aux_heat1", summary.AuxHeat1).
+		AddField("aux_heat2", summary.AuxHeat2).
+		AddField("aux_heat3", summary.AuxHeat3).
+		AddField("fan", summary.Fan).
+		AddField("idle", !summary.HeatPump && !summary.HeatPump2 && !summary.HeatPump3 && !summary.CompCool1 &&
+			!summary.CompCool2 && !summary.AuxHeat1 && !summary.AuxHeat2 && !summary.AuxHeat3 && !summary.Fan)
+	err = writeApi.WritePoint(context.Background(), thermostatPoint)
+	if err != nil {
+		fmt.Printf("Write failed: %v\n", err)
+	}
+
+	points := make([]*write.Point, len(thermostat.RemoteSensors)+1)
+	allOccupancy := false
+	for i, s := range thermostat.RemoteSensors {
+		point := write.NewPointWithMeasurement(config.InfluxDB.Measurements.Sensor).
+			SetTime(now)
+		points[i] = point
 		for _, c := range s.Capability {
 			if c.Type == "temperature" {
-				temp, err = strconv.ParseFloat(c.Value, 64)
+				temp, err := strconv.ParseFloat(c.Value, 64)
 				if err != nil {
-					log.Printf("Unable to parse temp %s", c.Value)
+					fmt.Printf("Unable to parse temp %s\n", c.Value)
 				} else {
-					temp = temp / 10
+					point.AddField("temperature", temp/10)
 				}
 			}
 			if c.Type == "occupancy" {
 				if c.Value == "true" {
-					occ = true
+					allOccupancy = true
+					point.AddField("occupancy", true)
 				}
 			}
 			if c.Type == "humidity" {
-				h, err := strconv.ParseFloat(c.Value, 64)
+				hum, err := strconv.ParseFloat(c.Value, 64)
 				if err != nil {
-					log.Printf("Unable to parse humidity %s", c.Value)
+					fmt.Printf("Unable to parse humidity %s\n", c.Value)
 				} else {
-					hum = &h
+					point.AddField("humidity", hum)
 				}
 			}
+			var name string
 			if strings.HasPrefix(s.ID, "rs:") {
 				name = fmt.Sprintf("EcobeeSensor: %s (%s)", s.Name, s.Code)
 			}
 			if strings.HasPrefix(s.ID, "ei:") {
 				name = fmt.Sprintf("EcobeeSensor: %s (Thermostat)", s.Name)
 			}
-		}
-		sensors[i] = Sensor{
-			Name: name,
-			SensorFields: SensorFields{
-				Temperature: temp,
-				Occupancy:   occ,
-				Humidity:    hum,
-			},
+			point.AddTag("name", name)
 		}
 	}
-	thermSensor := Sensor{
-		Name: "EcobeeTherm: " + t.Name,
-		SensorFields: SensorFields{
-			Temperature: float64(t.Runtime.ActualTemperature) / 10,
-			Occupancy:   AllOccupancy(sensors),
-		},
-	}
-	sensors[len(sensors)-1] = thermSensor
-
-	points := make([]*write.Point, 0)
-	addPoint := func(metric interface{}, name string, measurement string) {
-		point := FieldsToPoint(metric, now, name, measurement)
-		points = append(points, point)
-	}
-
-	for _, sensor := range sensors {
-		addPoint(sensor.SensorFields, sensor.Name, config.InfluxDB.Measurements.Sensor)
-	}
-	addPoint(therm, t.Name, config.InfluxDB.Measurements.Thermostat)
+	points[len(points)-1] = write.NewPointWithMeasurement(config.InfluxDB.Measurements.Sensor).
+		SetTime(now).
+		AddTag("name", "EcobeeTherm: "+thermostat.Name).
+		AddField("temperature", float64(thermostat.Runtime.ActualTemperature)/10).
+		AddField("occupancy", allOccupancy)
 	err = writeApi.WritePoint(context.Background(), points...)
 	if err != nil {
-		log.Printf("Write failed: %+v", errors.WithStack(err))
+		fmt.Printf("Write failed: %v\n", err)
 	}
-	lastRevision = s.RuntimeRevision
-	return
-}
-
-func FieldsToPoint(value interface{}, now time.Time, name string, measurement string) *write.Point {
-	tags := map[string]string{"name": name}
-	fields := make(map[string]interface{})
-	e := reflect.ValueOf(value)
-	for i := 0; i < e.NumField(); i++ {
-		name := strcase.ToSnake(e.Type().Field(i).Name)
-		field := e.Field(i)
-		if field.Kind() == reflect.Ptr && field.IsNil() {
-			// don't set value for nil pointers
-			continue
-		}
-		val := reflect.Indirect(field).Interface()
-		fields[name] = val
-	}
-	return write.NewPoint(measurement, tags, fields, now)
-}
-
-func EquipmentStatus(status ecobee.EquipmentStatus) string {
-	if status.HeatPump {
-		return "Heat1"
-	} else if status.HeatPump2 {
-		return "Heat2"
-	} else if status.HeatPump3 {
-		return "Heat3"
-	} else if status.CompCool1 {
-		return "Cool1"
-	} else if status.CompCool2 {
-		return "Cool2"
-	} else if status.AuxHeat1 {
-		return "Aux1"
-	} else if status.AuxHeat2 {
-		return "Aux2"
-	} else if status.AuxHeat3 {
-		return "Aux3"
-	} else if status.Fan {
-		return "Fan"
-	} else {
-		return "Idle"
-	}
-}
-
-func Program(t ecobee.Thermostat) string {
-	for _, e := range t.Events {
-		if e.Running {
-			switch e.Type {
-			case "vacation":
-				return "vacation"
-			case "hold":
-				return "hold"
-			}
-		}
-	}
-	return t.Program.CurrentClimateRef
-}
-
-func AllOccupancy(sensors []Sensor) bool {
-	for _, sensor := range sensors {
-		if sensor.Occupancy {
-			return true
-		}
-	}
-	return false
+	lastRevision = summary.RuntimeRevision
 }
 
 type Config struct {
@@ -287,32 +203,4 @@ type Config struct {
 		AuthCacheFile string `yaml:"auth_cache_file"`
 		PollCron      string `yaml:"poll_cron"`
 	}
-}
-
-type ThermostatFields struct {
-	CoolingSetpoint float64
-	HeatingSetpoint float64
-	Program         string // we will include vacation here, since program doesn't matter during vacation mode
-	EquipmentStatus string // Text version of bools for discrete graph
-	Heat1           bool
-	Heat2           bool
-	Heat3           bool
-	Cool1           bool
-	Cool2           bool
-	AuxHeat1        bool
-	AuxHeat2        bool
-	AuxHeat3        bool
-	Fan             bool
-	Idle            bool
-}
-
-type SensorFields struct {
-	Temperature float64
-	Occupancy   bool // note: may need string also for discrete graph?
-	Humidity    *float64
-}
-
-type Sensor struct {
-	Name string
-	SensorFields
 }
